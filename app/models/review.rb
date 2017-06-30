b69ab2d0893050142c18eb0c234471af866e0835
Ilene Gorski <ilene.gorski@gmail.com>
2017-06-30 16:55:57 -0700

    add reviews with faker

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,6 @@ class Review < ApplicationRecord
   validates :rating, numericality: { only_integer: true }
   validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
   validates :content_body, length: { in: 50..250 }
+  
   belongs_to :product
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,14 @@
 Product.destroy_all
 Review.destroy_all
 
-10.times do |index|
+50.times do |index|
   Product.create(name: Faker::Food.unique.ingredient, cost: [2.00, 4.50, 6.00, 8.50, 10.00].sample, country_of_origin: Faker::Address.country)
 end
+
+Product.all
+250.times do |index|
+  Review.create(content_body: Faker::Lorem.sentence(10, false, 0).chop, author: Faker::HarryPotter.character, rating: (1..5).to_a.sample, product_id: Product.all.sample.id)
+end
+
+
+Review.create(content_body: Faker::Lorem.sentence(10, false, 0).chop, author: Faker::HarryPotter.character, rating: (1..5).to_a.sample, product_id: Product.all.sample.id)
