f9df4bc685f3b873e842d33a8be9ff483f07642f
Ilene Gorski <ilene.gorski@gmail.com>
2017-06-30 15:16:46 -0700

    add scopes

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,15 @@ class Product < ApplicationRecord
   validates :name, :cost, :country_of_origin, :presence => true
   has_many :reviews
 
+  scope :most_popular, -> {
+    select("products.id, products.name, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group('products.id')
+    .order('reviews_count DESC')
+    .limit(3)
+  }
+
+  scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
+
+  scope :domestic, -> { where("country_of_origin like ?", "United States%")}
 end
